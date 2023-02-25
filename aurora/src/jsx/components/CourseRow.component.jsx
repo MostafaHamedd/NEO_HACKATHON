03b21ae2958a1b0427f4eb8ad712210a2c0f3203
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2023-02-25 17:30:53 -0600

    Fixed UI problems for section page

--- aurora/src/jsx/components/CourseRow.component.jsx
+++ aurora/src/jsx/components/CourseRow.component.jsx
@@ -6,12 +6,12 @@ export default class CourseRow extends Component {
   render() {
     const { courseName, courseFullName } = this.props;
     return (
-      <div className="row courseRow align-items-center">
-        <div className="col">{courseName}</div>
-        <div className="col">{courseFullName}</div>
-        <div className="col">3.00CR</div>
-        <Link className="col d-flex" to={URL_ROUTES.SECTIONS_PAGE}>
-          <button type="button" className="btn btn-primary ml-auto">
+      <div className="row courseRow align-items-center text-center">
+        <div className="col courseName">{courseName}</div>
+        <div className="col courseName">{courseFullName}</div>
+        <div className="col courseName">3.00CR</div>
+        <Link className="col d-flex" courseName to={URL_ROUTES.SECTIONS_PAGE}>
+          <button type="button " className="btn btn-primary ml-auto ">
             View Sections
           </button>
         </Link>
