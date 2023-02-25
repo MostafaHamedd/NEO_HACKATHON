243fffe2fc31c06b7e7c04f7e5a1ebe97bdf02ee
Jashan Gill <48613250+Jashann@users.noreply.github.com>
2023-02-25 15:12:44 -0600

    Data

--- aurora/src/resources/db/courses.data.js
+++ aurora/src/resources/db/courses.data.js
@@ -1,3 +1,27 @@
+const COMPSCI = new DEPARTMENT_COURSES();
+
+let subject = "COMP"
+let department = "COMPUTER SCIENCE"
+COMPSCI.addCourse(
+  new Course(
+    subject,
+    '1010',
+    '	Introductory Computer Science 1',
+    3,
+    'A01',
+    3126781,
+    'MWF',
+    '2:30AM - 3:30PM',
+    200,
+    190,
+    'Jack Ma',
+    '221 Wallace',
+    department 
+  )
+)
+
+
+
 class Course {
   constructor(
     subject,
@@ -11,7 +35,6 @@ class Course {
     capacity,
     active,
     instructor,
-    date,
     location,
     department
   ) {
@@ -26,24 +49,67 @@ class Course {
     this.capacity = capacity
     this.active = active
     this.instructor = instructor
-    this.date = date
     this.location = location
     this.department = department
   }
 
-  getSubject() { return this.subject }
-  getCourseCode() { return this.courseCode }
-  getTitle() { return this.title }
-  getCredits() { return this.credits }
-  getSectionNum() { return this.sectionNum }
-  getCRN() { return this.subject }
-  getDays() { return this.days }
-  getTime() { return this.time }
-  getCapacity() { return this.capacity }
-  getActive() { return this.active }
-  getInstructor() { return this.instructor }
-  getDate() { return this.date }
-  getLocation() { return this.location }
-  getDepartment() { return this.department }
+  getSubject() {
+    return this.subject
+  }
+  getCourseCode() {
+    return this.courseCode
+  }
+  getTitle() {
+    return this.title
+  }
+  getCredits() {
+    return this.credits
+  }
+  getSectionNum() {
+    return this.sectionNum
+  }
+  getCRN() {
+    return this.subject
+  }
+  getDays() {
+    return this.days
+  }
+  getTime() {
+    return this.time
+  }
+  getCapacity() {
+    return this.capacity
+  }
+  getActive() {
+    return this.active
+  }
+  getInstructor() {
+    return this.instructor
+  }
+  getLocation() {
+    return this.location
+  }
+  getDepartment() {
+    return this.department
+  }
 }
 
+class DEPARTMENT_COURSES {
+  constructor() {
+    this.COURSES = []
+  }
+
+  addCourse(newCourse) {
+    this.COURSES.push(newCourse)
+  }
+}
+
+class TERM {
+  constructor() {
+    this.DEPARTMENT = []
+  }
+
+  addDepartment(department) {
+    this.DEPARTMENT.push(department)
+  }
+}
