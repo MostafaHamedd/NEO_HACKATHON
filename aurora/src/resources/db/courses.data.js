70228044235f9fa48b2b15f40bf2c2bf1bd70c0f
Jashan Gill <48613250+Jashann@users.noreply.github.com>
2023-02-25 15:00:56 -0600

    Data

--- aurora/src/resources/db/courses.data.js
+++ aurora/src/resources/db/courses.data.js
@@ -35,39 +35,3 @@ class Course {
   getSubject() { return this.subject }
 }
 
-const NAMES_TERMS = {
-  "Summer 2023": "Summer 2023",
-  "Winter 2023": "Winter 2023",
-  "Fall 2023": "Fall 2023",
-};
-
-const NAMES_COURSES = {
-  "COMP 1012": "COMP 1012",
-};
-
-const COURSES = [
-  TERM_NAMES["Summer 2023"],
-  TERM_NAMES["Winter 2023"],
-  TERM_NAMES["Fall 2023"],
-];
-
-const COURSES_INFO = {};
-
-class terms {
-  constructor(courses, examDates){
-    this.courses = courses;
-    this.examDates = examDates;
-    this.coursesList = [];
-    this.courseCount = 0;
-  }
-  
-  addCourses (course) {
-    if (this.courseCount > 5 && hasDuplicates(this.coursesList)) {
-      console.log("Error you may not have more than 5 courses.");
-    } else {
-      this.coursesList.push(course);
-      this.courseCount ++;
-    }
-
-  }
-}
