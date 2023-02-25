c0bf2fb913467319665c22e40a574b12764d84ec
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2023-02-25 17:38:23 -0600

    Added more courses to section page

--- aurora/src/jsx/pages/CourseList.page.jsx
+++ aurora/src/jsx/pages/CourseList.page.jsx
@@ -17,6 +17,21 @@ export default function CourseListPage() {
         courseName="COMP 2140"
         courseFullName="DATA STRUCTURES AND ALGORITHMS"
       />
+      <CourseRow courseName="COMP 2150" courseFullName="OBJECT ORIENTATION" />
+      <CourseRow
+        courseName="COMP 2160"
+        courseFullName="PROGRAMMING PRACTICES"
+      />
+      <CourseRow
+        courseName="COMP 2280"
+        courseFullName="INTRODUCTION TO COMPUTER SYSTEMS"
+      />
+      <CourseRow
+        courseName="COMP 2080"
+        courseFullName="ANALYSIS OF ALGORITHMS"
+      />
+      <CourseRow courseName="COMP 4380" courseFullName="DATBASES II" />
+      <CourseRow courseName="COMP 4710" courseFullName="DATA MINING" />
     </div>
   );
 }
